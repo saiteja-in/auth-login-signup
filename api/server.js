62d0473113c88a47a0196ffa9201bff88ee35677
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-09 23:23:36 +0530

    create signup api route

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+
 dotenv.config();
 
 mongoose
@@ -10,6 +13,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json())
 const PORT = 3000;
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
