2d19dc45e6db7b9109a9732cf0fec7f7e64cbecc
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-09 21:53:40 +0530

    created routes(client side)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+const App = () => {
   return (
-    <h1 className="text-red-500">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
