4a943202dc52dfb8b21f75cf26cffb978d6b8e4e
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-15 22:06:31 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {Provider} from 'react-redux';
-import { store } from './redux/store.js';
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './user/userSlice'
-export const store =configureStore({
-    reducer:{users:userReducer},
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-        serializableCheck:false,
-    })
-})
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Combine all reducers
+const rootReducer = combineReducers({
+  user: userReducer
+});
+
+// Configure persist settings
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the Redux store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+// Create a persistor
+export const persistor = persistStore(store);
